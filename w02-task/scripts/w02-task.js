2c60897be54057423f712b9f767ec92b07a284a1
Your Name <you@example.com>
2023-09-21 10:37:06 -0700

    w2

--- w02-task/scripts/w02-task.js
+++ w02-task/scripts/w02-task.js
@@ -3,11 +3,19 @@
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
+let fullName = 'Curtis J. Price';
+
+let currentYear = '#year';
+
+let profilePicture = 'images/IMG_20230911_132428.jpg';
 
 
 
 
 /* Step 3 - Element Variables */
+const nameElement = document.getElementById('name');
+
+const yearElement = document.querySelector('#year');
 
 
 
