5377d73feaed1e0b81423560c5e4baa5cfe5a0d5
Your Name <you@example.com>
2023-09-30 01:57:41 -0700

    progress

--- w02-task/scripts/w03-task.js
+++ w02-task/scripts/w03-task.js
@@ -9,15 +9,38 @@
 
 
 /* Function Expression - Subtract Numbers */
+function subtract(number1, number2){
+    const difference = number1 - number2
+    return difference
+}
 
 
 /* Arrow Function - Multiply Numbers */
+function multiply(number1, number2){
+    const product = number1 * number2
+    return product
+}
 
 
 /* Open Function Use - Divide Numbers */
+function divide(number1, number2){
+    const quotient = number1 / number2
+    return quotient
+}
 
 
 /* Decision Structure */
+function addNumbers(){
+    const firstNumber = document.querySelector("#addend1").value
+    const secondNumber = document.querySelector("#addend2").value
+    const sum = add(firstNumber,secondNumber)
+    document.querySelector("#sum").value = sum
+    
+        console.log("First Number:", firstNumber)
+        console.log("Second Number:", secondNumber)
+        console.log("Sum", sum)
+}
+document.querySelector("#addNumbers").addEventListener('click', addNumbers)
 
 
 /* ARRAY METHODS - Functional Programming */
