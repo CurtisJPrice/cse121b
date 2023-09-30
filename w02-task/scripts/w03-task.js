0ab2a584ed8b8826e12875cc1c257aa57935f72f
Your Name <you@example.com>
2023-09-30 02:14:00 -0700

    fix

--- w02-task/scripts/w03-task.js
+++ w02-task/scripts/w03-task.js
@@ -40,18 +40,95 @@ function addNumbers(){
         console.log("Second Number:", secondNumber)
         console.log("Sum", sum)
 }
-document.querySelector("#addNumbers").addEventListener('click', addNumbers)
+document.querySelector("#addNumbers").addEventListener('click', addNumbers);
 
 
-/* ARRAY METHODS - Functional Programming */
+const subtract = function (number1, number2) {
+    return number1 - number2;
+};
+
+const subtractNumbers = function () {
+    let minuend = parseInt(document.querySelector("#minuend").value);
+    let subtrahend = parseInt(document.querySelector("#subtrahend").value);
+    let result = subtract(minuend, subtrahend);
+    document.querySelector("#difference").value = result;
+};
+
+document
+    .querySelector("#subtractNumbers")
+    .addEventListener("click", subtractNumbers);
+
+
+    const multiply = (number1, number2) => number1 * number2;
+
+    const multiplyNumbers = () => {
+    let factor1 = parseInt(document.querySelector("#factor1").value);
+    let factor2 = parseInt(document.querySelector("#factor2").value);
+    let result = multiply(factor1, factor2);
+    document.querySelector("#product").value = result;
+};
+
+document
+    .querySelector("#multiplyNumbers")
+    .addEventListener("click", multiplyNumbers);
+
+
+function divide(number1, number2) {
+    return number1 / number2;
+}
+
+function divideNumbers() {
+    let dividend = parseInt(document.querySelector("#dividend").value);
+    let divisor = parseInt(document.querySelector("#divisor").value);
+    let result = divide(dividend, divisor);
+    document.querySelector("#quotient").value = result;
+}
+
+document
+    .querySelector("#divideNumbers")
+    .addEventListener("click", divideNumbers);
+
+
+const currentDate = new Date();
+
+let currentYear;
+
+currentYear = currentDate.getFullYear();
+
+document.querySelector("#year").textContent = currentYear;
+
+
+  /* ARRAY METHODS - Functional Programming */
 /* Output Source Array */
+const numbers = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
+    23, 24, 25
+];
+
+document.querySelector("#array").textContent = numbers;
 
 /* Output Odds Only Array */
+document.querySelector("#odds").textContent = numbers.filter(
+    (number) => number % 2
+);
 
 /* Output Evens Only Array */
+document.querySelector("#evens").textContent = numbers.filter(
+    (number) => number % 2 === 0
+);
 
 /* Output Sum of Org. Array */
+document.querySelector("#sumOfArray").textContent = numbers.reduce(
+    (sum, number) => sum + number,
+    0
+);
 
 /* Output Multiplied by 2 Array */
+document.querySelector("#multiplied").textContent = numbers.map(
+    (number) => number * 2
+);
 
 /* Output Sum of Multiplied by 2 Array */
+document.querySelector("#sumOfMultiplied").textContent = numbers
+    .map((number) => number * 2)
+    .reduce((sum, number) => sum + number, 0);
